4e25b567661bb3fead3a19a787bd5142ac1e6304
unknown <testingofficial002@gmail.com>
2026-02-27 14:10:55 +0000

    Commit Updated config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -53,7 +53,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-  /*  {
+   {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
@@ -61,7 +61,7 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },*/
+    },
 
     /* Test against mobile viewports. */
     // {
